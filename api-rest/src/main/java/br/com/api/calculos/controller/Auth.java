57559028b551d5d721039294a641cccb630e86e7
fmachado <fernando.machado@bs.nttdata.com>
2024-04-25 23:43:56 -0300

    [6] - Implementação de rotinas para autenticação e geração de token

---
+++ api-rest/src/main/java/br/com/api/calculos/controller/Auth.java
@@ -0,0 +1,31 @@
+package br.com.api.calculos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.calculos.bo.TokenBO;
+import br.com.api.calculos.service.AuthService;
+
+@RestController
+@RequestMapping("/auth")
+public class Auth {
+    
+    @Autowired
+    private AuthService service;
+
+    @GetMapping(
+        value = "/token/{user}/{password}",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public TokenBO gerarToken(
+        @PathVariable(value = "user") String user,
+        @PathVariable(value = "password") String password
+    ){
+        return service.gerarToken(user, password);
+    }
+
+}
