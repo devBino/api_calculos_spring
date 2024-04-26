15266f2936da33b1d12f9079c9796d9ac9c8e201
fmachado <fernando.machado@bs.nttdata.com>
2024-04-26 07:36:48 -0300

    [9] - Adicionando paginação nos end poitns de listagem

--- api-rest/src/main/java/br/com/api/calculos/controller/CalculoController.java
+++ api-rest/src/main/java/br/com/api/calculos/controller/CalculoController.java
@@ -3,6 +3,9 @@ package br.com.api.calculos.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.calculos.bo.CalculoBO;
@@ -47,9 +51,18 @@ public class CalculoController {
         return service.atualizar(body);
     }
 
-    @GetMapping(value = "/listar", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<CalculoBO> listar(){
-        return service.listar();
+    @GetMapping(
+        value = "/listar", 
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Page<CalculoBO>> listar(
+        @RequestParam(value = "page", defaultValue = "1") Integer page,
+        @RequestParam(value = "limit", defaultValue = "10") Integer limit
+    ){
+        
+        final Pageable paginacao = PageRequest.of(--page, limit);
+        return ResponseEntity.ok(service.listar(paginacao));
+
     }
 
     @GetMapping(value = "/listar-por-sinal/{sinal}")
