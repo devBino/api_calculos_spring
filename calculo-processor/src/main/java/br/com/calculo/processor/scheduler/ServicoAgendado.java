79bfc9ec5750c1078fda69451aa887d5402b180c
devBino <programadormachado@gmail.com>
2024-06-21 10:20:19 -0300

    [41] - Revisando solução para concorrência em multithreading no serviço de processamento dos calculos

--- calculo-processor/src/main/java/br/com/calculo/processor/scheduler/ServicoAgendado.java
+++ calculo-processor/src/main/java/br/com/calculo/processor/scheduler/ServicoAgendado.java
@@ -2,28 +2,36 @@ package br.com.calculo.processor.scheduler;
 
 import static br.com.calculo.processor.constants.ProcessoConstants.*;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.Executor;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import br.com.calculo.processor.service.CalculoService;
 
+/**
+ * Agendador do serviço de processamento 
+ * dos calculos
+ */
 @Component
 public class ServicoAgendado {
     
     @Autowired
     private CalculoService service;
 
+    @Autowired
+    @Qualifier("asyncExecutor")
+    private Executor asynExecutor;
+
+    @Async("asyncExecutor")
     @Scheduled(fixedRate = INTERVALO_SERVICO)
     public void executarProcesso(){
         
-        final ExecutorService exec = Executors.newFixedThreadPool(QTDE_THREADS);
-
         for(int i=0; i<QTDE_THREADS; i++){
-            exec.submit( () -> service.processarCalculo() );
+            service.processarCalculo();
         }
 
     }
--- calculo-processor/src/main/java/br/com/calculo/processor/service/CalculoService.java
+++ calculo-processor/src/main/java/br/com/calculo/processor/service/CalculoService.java
@@ -2,9 +2,13 @@ package br.com.calculo.processor.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.calculo.processor.model.MCalculo;
@@ -25,6 +29,10 @@ public class CalculoService implements RegistroService {
     private MCalculo calculo;
 
     @Autowired
+    @Qualifier("asyncExecutor")
+    private Executor asyncExecutor;
+
+    @Autowired
     private CalculoRepository calculoRepository;
 
     @Autowired
@@ -34,20 +42,27 @@ public class CalculoService implements RegistroService {
 
     @Transactional
     public void processarCalculo(){
-        
+        aplicarEtapasProcessamento();
+    }
+
+    @Async
+    private void aplicarEtapasProcessamento(){
+
         calculo = null;
         mensagens = new ArrayList<>();
 
         if(!getRegistro()){
             return;
         }
-
+        System.out.println( Thread.currentThread().getName() + " iniciou id " + calculo.getId() );
         mensagens.add(new MensagemProcessVO(MensagemHistoricoType.INFO, 
             "Calculo " + calculo.getId() + " identificado", calculo.getId()));
 
         prepararRegistro();
         processarRegistro();
         gerarHistoricos();
+        System.out.println( Thread.currentThread().getName() + " finalizou id " + calculo.getId() );
+        System.out.println("*************************************************************************************");
 
     }
 
@@ -55,7 +70,6 @@ public class CalculoService implements RegistroService {
     public boolean getRegistro(){
         calculo = calculoRepository.findByEstado('A');
         return calculo != null;
-
     }
 
     @Override
@@ -70,43 +84,42 @@ public class CalculoService implements RegistroService {
 
     @Override
     public void processarRegistro(){
-        aplicarCalculo(calculo);
+        aplicarCalculo();
         calculoRepository.save(calculo);
         mensagens.add(new MensagemProcessVO(MensagemHistoricoType.INFO, 
             "Finalizado Processo", calculo.getId()));
     }
 
-    private void aplicarCalculo(final MCalculo pMCalculo){
+    private void aplicarCalculo(){
 
         mensagens.add(new MensagemProcessVO(MensagemHistoricoType.INFO, 
             "Calculando parametros", calculo.getId()));
 
-        if( pMCalculo.getSinal() == '+' ){
-            pMCalculo.setResultado( pMCalculo.getNumero1() + pMCalculo.getNumero2() );
-        }else if( pMCalculo.getSinal() == '-' ){
-            pMCalculo.setResultado( pMCalculo.getNumero1() - pMCalculo.getNumero2() );
-        }else if( pMCalculo.getSinal() == '*' ){
-            pMCalculo.setResultado( pMCalculo.getNumero1() * pMCalculo.getNumero2() );
-        }else if( pMCalculo.getSinal() == '/' ){
-            pMCalculo.setResultado( pMCalculo.getNumero1() / pMCalculo.getNumero2() );
+        if( calculo.getSinal() == '+' ){
+            calculo.setResultado( calculo.getNumero1() + calculo.getNumero2() );
+        }else if( calculo.getSinal() == '-' ){
+            calculo.setResultado( calculo.getNumero1() - calculo.getNumero2() );
+        }else if( calculo.getSinal() == '*' ){
+            calculo.setResultado( calculo.getNumero1() * calculo.getNumero2() );
+        }else if( calculo.getSinal() == '/' ){
+            calculo.setResultado( calculo.getNumero1() / calculo.getNumero2() );
         }
 
         final StringBuilder sb = new StringBuilder()
-                .append(pMCalculo.getNumero1())
+                .append(calculo.getNumero1())
                 .append(" ")
-                .append(pMCalculo.getSinal())
+                .append(calculo.getSinal())
                 .append(" ")
-                .append(pMCalculo.getNumero2())
+                .append(calculo.getNumero2())
                 .append(" = ")
-                .append(pMCalculo.getResultado());
+                .append(calculo.getResultado());
 
-                
-        pMCalculo.setDescricao(sb.toString());
+        calculo.setDescricao(sb.toString());
         
         mensagens.add(new MensagemProcessVO(MensagemHistoricoType.INFO, 
             "Calculo [" + calculo.getDescricao() + "] bem sucedido", calculo.getId()));
 
-        pMCalculo.setEstado('F');
+        calculo.setEstado('F');
 
     }
 
