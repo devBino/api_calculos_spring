cc668555e78d2312754655a41579c035677c379d
devBino <programemachado@gmail.com>
2024-06-30 22:09:02 -0300

    [148] - Ajustes para atender CI React

--- clients/react/app-calculos/src/routes.js
+++ clients/react/app-calculos/src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Login from './pages/Login'
 import Cadastro from './pages/Cadastro';
