ac07e4009e6497727622c3b23d218d1227c1a081
devBino <programemachado@gmail.com>
2024-06-30 22:13:33 -0300

    [148] - Ajustes para atender CI React

--- clients/react/app-calculos/src/pages/DivContainer/index.js
+++ clients/react/app-calculos/src/pages/DivContainer/index.js
@@ -16,7 +16,7 @@ export default function DivContainer({children, title, login}){
             <div class="row">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Calculos</a>
+                    <Link className="button" class="navbar-brand" to="/calculos">Calculos</Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
