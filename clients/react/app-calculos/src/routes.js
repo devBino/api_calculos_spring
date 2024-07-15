b00c9551b3a1fa7f74ec19922c231d874a534205
devBino <programemachado@gmail.com>
2024-07-15 20:37:58 -0300

    feat:[216] - Mensagem padrão e redirect para páginas não encontradas

--- clients/react/app-calculos/src/routes.js
+++ clients/react/app-calculos/src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import Login from './pages/Login'
 import Cadastro from './pages/Cadastro';
@@ -9,6 +9,7 @@ import Calculos from './pages/Calculos';
 import CalculoDetalhe from './pages/CalculoDetalhe';
 import Anexos from './pages/Anexos';
 import CalculosAnexo from './pages/CalculosAnexo';
+import NotFound from './pages/NotFound';
 
 export default function RoutesApp(){
     return (
@@ -22,6 +23,10 @@ export default function RoutesApp(){
                 <Route path='/calculo-detalhe' element={<CalculoDetalhe/>} > </Route>
                 <Route path="/anexos" element={<Anexos/>} > </Route>
                 <Route path="/calculos-anexo" element={<CalculosAnexo/>} > </Route>
+
+                <Route path="*" element={<Navigate to="/pagina-nao-encontrada" />} />
+                <Route path="/pagina-nao-encontrada" element={<NotFound />} />
+                
             </Routes>
         </Router>
     );  
